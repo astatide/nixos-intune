2951fabe13bfc6d43379d115224edee3730967d9
Audrey MP <9011779+astatide@users.noreply.github.com>
2022-04-01 14:04:26 -0400

    Update msft-identity-broker.nix

--- intune/msft-identity-broker.nix
+++ intune/msft-identity-broker.nix
@@ -22,10 +22,6 @@ let
       sha256 = "1frbqsvsz74w1sms2sdbn402b2aabp4v354g4qs2nl0agcnvw8sm";
   };
 
-  #libsdbus = pkgs.callPackage /etc/nixos/features/intune/libsdbus.nix {};
-  #msaldk = pkgs.callPackage /etc/nixos/features/intune/msaldk.nix {};
-
-
 in stdenv.mkDerivation {
   name = "msft-identity-broker-${version}";
   pname = "msft-identity-broker";
@@ -63,34 +59,8 @@ in stdenv.mkDerivation {
 
     cp -av $TMP/usr/* $out/
     cp -av $TMP/opt $out/
-    #cp -av ${coreutils}/bin/which $out/opt/msft/identitybroker/bin/
     
-    # fix the dbus and systemd files to be in their propr place.
-    #sed -i "s|/opt/msft/identitybroker/|$out/opt/msft/identitybroker/|g" $out/lib/systemd/system/msft-identity-device-broker.service
-    #sed -i "s|/opt/msft/identitybroker/|$out/opt/msft/identitybroker/|g" $out/lib/systemd/user/msft-identity-broker.service
-    #sed -i "s|/opt/msft/identitybroker/|$out/opt/msft/identitybroker/|g" $out/share/dbus-1/system-services/com.microsoft.identity.devicebroker1.service
-    #sed -i "s|/opt/msft/identitybroker/|$out/opt/msft/identitybroker/|g" $out/share/dbus-1/services/com.microsoft.identity.broker1.service
-    #echo User=root >> $out/share/dbus-1/system-services/com.microsoft.identity.devicebroker1.service
-    #rm $out/share/dbus-1/services/com.microsoft.identity.broker1.service $out/lib/systemd/system/msft-identity-device-broker.service $out/lib/systemd/user/msft-identity-broker.service
-    #rm $out/share/dbus-1/system-services/com.microsoft.identity.devicebroker1.service
-
-    #wrapProgram $out/opt/msft/identitybroker/bin/msft-identity-device-broker \
-    #--prefix PATH : ${coreutils}/bin \
-    #--set MSFT_IDENTITY_BROKER_OPTS "-verbose" \
-    #--set JAVA_HOME ${jdk11_headless.home} \
-    #--prefix LD_LIBRARY_PATH : ${pkgs.gtk3}/lib 
-
-    #wrapProgram $out/opt/msft/identitybroker/bin/msft-identity-broker \
-    #--prefix PATH : ${coreutils}/bin \
-    #--set MSFT_IDENTITY_BROKER_OPTS "-verbose" \
-    #--set JAVA_HOME ${jdk11_headless.home} \
-    #--prefix LD_LIBRARY_PATH : ${pkgs.gtk3}/lib 
-
-    #--set DBUS_SESSION_BUS_ADDRESS "unix:path=$dbus_socket_dir/session" 
   '';
-
-  #     --prefix XDG_DATA_DIRS : ${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name} \
-
 
   meta = with lib; {
     description = "msft-identity-broker";
